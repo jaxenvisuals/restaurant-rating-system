2e8e8681913b03905c0d5cad9bfde52f51442c80
Ogunmasa Jackson <42275320+jaxenvisuals@users.noreply.github.com>
2021-12-14 13:26:01 +0000

    Star ratings should allow bigger star size props

--- components/utilities/ratings/StarRating.vue
+++ components/utilities/ratings/StarRating.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="flex items-center">
     <div class="flex items-center mr-1 text-brand-yellow">
-      <MaterialIcon icon="star" class="text-xs" />
-      <MaterialIcon icon="star" class="text-xs" />
-      <MaterialIcon icon="star" class="text-xs" />
-      <MaterialIcon icon="star_half" class="text-xs" />
-      <MaterialIcon icon="star_outline" class="text-xs" />
+      <MaterialIcon icon="star" :class="[starSize]" />
+      <MaterialIcon icon="star" :class="[starSize]" />
+      <MaterialIcon icon="star" :class="[starSize]" />
+      <MaterialIcon icon="star_half" :class="[starSize]" />
+      <MaterialIcon icon="star_outline" :class="[starSize]" />
     </div>
-    <span class="text-[10px] text-brand-gray leading-snug tracking-tight"
+    <span
+      v-if="count"
+      class="text-[10px] text-brand-gray leading-snug tracking-tight"
       >2.4k</span
     >
   </div>
@@ -16,5 +18,34 @@
 <script>
 export default {
   name: 'StarRatings',
+
+  props: {
+    count: {
+      type: Boolean,
+      default: true,
+    },
+
+    size: {
+      type: String,
+      default: null,
+    },
+  },
+
+  computed: {
+    starSize() {
+      let size = null
+      switch (this.size) {
+        case 'md':
+          size = 'text-base'
+          break
+
+        default:
+          size = 'text-xs'
+          break
+      }
+
+      return size
+    },
+  },
 }
 </script>
